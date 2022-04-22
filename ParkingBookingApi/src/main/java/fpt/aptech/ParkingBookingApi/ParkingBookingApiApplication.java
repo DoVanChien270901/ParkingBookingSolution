c8297093bd5b251bfe356579723661ada2d0e3a3
CHIEN <CHIEN@192.168.111.2>
2022-04-22 19:13:49 +0700

    Innit Solution

---
+++ ParkingBookingApi/src/main/java/fpt/aptech/ParkingBookingApi/ParkingBookingApiApplication.java
@@ -0,0 +1,13 @@
+package fpt.aptech.ParkingBookingApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingBookingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingBookingApiApplication.class, args);
+	}
+
+}
---
+++ ParkingBookingApi/src/test/java/fpt/aptech/ParkingBookingApi/ParkingBookingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package fpt.aptech.ParkingBookingApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingBookingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
