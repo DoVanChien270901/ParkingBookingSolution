db10cde5b86bf1a88658b30fc23e749d5f7e0588
CHIEN <CHIEN@192.168.111.2>
2022-04-22 19:15:28 +0700

    Innit Web Application

---
+++ ParkingBookingApplication/src/test/java/fpt/aptech/ParkingBookingApplicatiton/ParkingBookingApplicatitonApplicationTests.java
@@ -0,0 +1,13 @@
+package fpt.aptech.ParkingBookingApplicatiton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingBookingApplicatitonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
